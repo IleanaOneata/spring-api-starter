c85d642d952661def4b08ac772a594f3936169b5
Ileana Oneata <ileana.oneata@yahoo.com>
2025-04-23 21:24:15 +0300

    Building an API

---
+++ src/main/java/com/codewithmosh/store/entities/Message.java
@@ -0,0 +1,16 @@
+package com.codewithmosh.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author ileanaoneata on 23.04.2025
+ */
+
+@AllArgsConstructor
+@Getter
+public class Message {
+
+    private String message;
+
+}
