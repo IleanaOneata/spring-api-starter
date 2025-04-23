b9bbf0a6f7e74ec036db4bf4344ee771b06bd575
Ileana Oneata <ileana.oneata@yahoo.com>
2025-04-23 21:32:38 +0300

    Creating first RESTful API

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ileanaoneata on 23.04.2025
+ */
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
