1bbffd41eef569c048c87a10e2895cbb155beafa
Ileana Oneata <ileana.oneata@yahoo.com>
2025-05-08 22:02:30 +0300

    Introduction to Jakarta Validations

--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,5 +1,8 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -9,7 +12,15 @@ import lombok.Data;
 @Data
 public class RegisterUserRequest {
 
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 25, message = "Password must be between 6 to 25 characters long.")
     private String password;
 }
