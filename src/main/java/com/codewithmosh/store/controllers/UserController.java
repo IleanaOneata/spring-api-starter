1bbe634f6905b09ff8fd610116c28ba741c8373a
Ileana Oneata <ileana.oneata@yahoo.com>
2025-05-08 20:06:17 +0300

    Extracting Request Body

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -47,4 +47,9 @@ public class UserController {
 
         return ResponseEntity.ok(userMapper.toDto(user));
     }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto data) {
+        return data;
+    }
 }
