07490f220cd5adc1029d93f8f649d1aeedcf8cd7
Ileana Oneata <ileana.oneata@yahoo.com>
2025-05-08 20:33:06 +0300

    Updating Resources

--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,9 +1,11 @@
 package com.codewithmosh.store.mappers;
 
 import com.codewithmosh.store.dtos.RegisterUserRequest;
+import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 /**
  * @author ileanaoneata on 23.04.2025
@@ -15,4 +17,6 @@ public interface UserMapper {
     UserDto toDto(User user);
 
     User  toEntity(RegisterUserRequest request);
+
+    void update(UpdateUserRequest request, @MappingTarget User user);
 }
