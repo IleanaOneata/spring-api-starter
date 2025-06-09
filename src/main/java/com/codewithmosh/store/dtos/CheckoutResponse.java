d5c243d35b023b9d5c66a8710a00bd09c891d22e
Ileana Oneata <ileana.oneata@yahoo.com>
2025-06-09 19:16:13 +0300

    Creating a Checkout Session

--- src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutResponse.java
@@ -10,8 +10,10 @@ import lombok.Data;
 public class CheckoutResponse {
 
     private Long orderId;
+    private String checkoutUrl;
 
-    public CheckoutResponse(Long orderId) {
+    public CheckoutResponse(Long orderId, String checkoutUrl) {
         this.orderId = orderId;
+        this.checkoutUrl = checkoutUrl;
     }
 }
