4eb29b5a43262f231f1111806c756f78eeada222
Ileana Oneata <ileana.oneata@yahoo.com>
2025-06-09 15:29:02 +0300

    Extracting a Service

--- src/main/java/com/codewithmosh/store/exceptions/CartNotFoundException.java
+++ src/main/java/com/codewithmosh/store/exceptions/CartNotFoundException.java
@@ -5,4 +5,7 @@ package com.codewithmosh.store.exceptions;
  */
 public class CartNotFoundException extends RuntimeException {
 
+    public CartNotFoundException() {
+        super("Cart not found");
+    }
 }
